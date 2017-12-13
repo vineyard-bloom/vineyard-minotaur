9807a83e711dd50c61e264424d50637594cb16e8
dylanavery <fossilfuelmusic@gmail.com>
2017-12-13 16:33:06 -0700

    tsc

--- src/deposit-monitor.js
+++ src/deposit-monitor.js
@@ -126,7 +126,7 @@ class DepositMonitor {
     }
     update() {
         return __awaiter(this, void 0, void 0, function* () {
-            const block = yield this.client.getLastBlock();
+            const block = yield this.client.getBlockIndex();
             yield this.updatePendingTransactions(block.index - this.minimumConfirmations);
             yield this.scanBlocks();
         });
