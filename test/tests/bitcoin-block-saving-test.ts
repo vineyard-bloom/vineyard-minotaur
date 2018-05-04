89c394e432f24ba75964dc6cc02d34749ec26225
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-05-04 15:15:59 -0600

    Add block creation to blocking saving test

--- test/tests/bitcoin-block-saving-test.ts
+++ test/tests/bitcoin-block-saving-test.ts
@@ -7,14 +7,21 @@ import { BitcoinBlockReader } from "vineyard-bitcoin/src/bitcoin-block-reader"
 
 describe('bitcoin block saving test', function () {
   
-    it('can get a block by index', async function () {
-      const village = await createBitcoinVillage(bitcoinConfig, BitcoinBlockReader.createFromConfig(bitcoinConfig.bitcoin))
-      const { model, client } = village
-      const dao = createBitcoinExplorerDao(model)
+  it('can get a block by index', async function () {
+    const village = await createBitcoinVillage(bitcoinConfig, BitcoinBlockReader.createFromConfig(bitcoinConfig.bitcoin))
+    const { model, client } = village
+    const dao = createBitcoinExplorerDao(model)
+    // Create a new block of type BaseBlock
+    const newBlock = {
+      hash: 'hashstring',
+      index: 1,
+      // Not sure how to format the Date
+      timeMined: Date
+    }
+    // Add the block
+    dao.blockDao.saveBlock(newBlock)
 
-
-      const blockExists = await checkIfBlockSaved(dao, block)
-      assert(blockExists)
-    })
-
+    const blockExists = await checkIfBlockSaved(dao, block)
+    assert(blockExists)
   })
+})
