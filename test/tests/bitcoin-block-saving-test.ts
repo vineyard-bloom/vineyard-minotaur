c800a846c709af5dd3932513d717f89a7d323dbd
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-05-07 13:27:47 -0600

    Refactor dao creation in block saving test

--- test/tests/bitcoin-block-saving-test.ts
+++ test/tests/bitcoin-block-saving-test.ts
@@ -1,6 +1,6 @@
 import { assert } from 'chai'
 import { checkBlockScanStatus } from "../../src";
-import { BitcoinModel, createBitcoinExplorerDao } from '../../src/bitcoin-explorer/bitcoin-model';
+import { BitcoinModel, createBitcoinExplorerDao, BitcoinMonitorDao } from '../../src/bitcoin-explorer/bitcoin-model';
 import { createBitcoinVillage } from "../../lab/bitcoin-explorer-service"
 import { bitcoinConfig } from "../../config/config-btc"
 import { BitcoinBlockReader } from "vineyard-bitcoin/src/bitcoin-block-reader"
@@ -9,22 +9,24 @@ import { ScannedBlockStatus } from "../../src/bitcoin-explorer/bitcoin-explorer"
 
 describe('bitcoin block saving test', function () {
   let model: BitcoinModel
+  let dao: BitcoinMonitorDao
 
-  beforeEach(async () => {
+  before(async() => {
     const village = await createBitcoinVillage(bitcoinConfig, BitcoinBlockReader.createFromConfig(bitcoinConfig.bitcoin))
     model = village.model
+    dao = createBitcoinExplorerDao(model)
+  })
+
+  beforeEach(async () => {
     await (model.ground as DevModeler).regenerate()
   })
 
   it('can detect when block has been rescanned with no changes', async function () {
-    const dao = createBitcoinExplorerDao(model)
-    // Create a new block of type BaseBlock
     const newBlock = {
       hash: 'hashstring',
       index: 1,
       timeMined: new Date()
     }
-    // Add the block
     const block = await model.Block.create(newBlock)
 
     const scannedBlockStatus = await checkBlockScanStatus(dao, block)
@@ -32,14 +34,11 @@ describe('bitcoin block saving test', function () {
   })
 
   it('can detect when block has  been rescanned with changes', async function () {
-    const dao = createBitcoinExplorerDao(model)
-    // Create a new block of type BaseBlock
     const newBlock = {
       hash: 'originalHash',
       index: 1,
       timeMined: new Date()
     }
-    // Add the block
     const block = await model.Block.create(newBlock)
 
     const scannedBlockStatus = await checkBlockScanStatus(dao, {index: block.index, hash: 'newHash'})
@@ -47,8 +46,6 @@ describe('bitcoin block saving test', function () {
   })
 
   it('can detect when block has never been scanned', async function () {
-    const dao = createBitcoinExplorerDao(model)
-
     const scannedBlockStatus = await checkBlockScanStatus(dao, {index: 1, hash: 'originalHash'})
     assert.equal(ScannedBlockStatus.Nonexistent, scannedBlockStatus)
   })
